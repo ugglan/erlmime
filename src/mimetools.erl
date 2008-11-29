5796466f2e379a527531707d0a698717339c22b1
Tobias Löfgren <ugglan@gmail.com>
2008-11-29 23:21:35 +0100

    As I interpret RFC 2045, newlines should be CRLF. This implementation does it's best to adapt to that.

--- src/mimetools.erl
+++ src/mimetools.erl
@@ -12,16 +12,20 @@ encode(quoted_printable, IoList) -> encode_qp(lists:flatten(IoList), [], 0);
 encode(base64, IoList) -> encode_b64(iolist_to_binary(IoList)).
 
 %%
-%%  Quoted printable encoding as specified in RFC 2045
+%% Quoted printable encoding as specified in RFC 2045.
+%%
+%% This implementation changes unix newline (LF) to CRLF. A single CR
+%% is converted to =0D unless directly followed by a LF. And CRLF is
+%% kept as CRLF. This means that it is not suitable for non-text data.
 %%
 
 encode_qp([], Acc, Width) when Width<77 -> lists:reverse(Acc);
 
-encode_qp([$\n|In], Acc, _Width) ->  encode_qp(In, [$\n | Acc], 0);
-encode_qp([$\r,$\n|In], Acc, _Width) -> encode_qp(In, [$\n | Acc], 0);
+encode_qp([$\n|In], Acc, _Width) ->  encode_qp(In, [$\n,$\r | Acc], 0);
+encode_qp([$\r,$\n|In], Acc, _Width) -> encode_qp(In, [$\n,$\r | Acc], 0);
 
 encode_qp(In, [L|Acc], Width) when Width>75 ->
-    encode_qp(In, [L,$\n,$= | Acc], 
+    encode_qp(In, [L,$\n,$\r,$= | Acc], 
 	   case is_list(L) of true -> 3; false -> 1 end );
 
 encode_qp([C|In], Acc, Width) when C>32, C<127, C=/=61 -> 
@@ -57,7 +61,7 @@ encode_b64(In) ->
 encode_b64(<<>>, Acc) -> lists:reverse(Acc);
 
 encode_b64(<<Row:76/binary, Rest/binary>>, Acc) ->
-    encode_b64(Rest, [$\n, Row | Acc]);
+    encode_b64(Rest, [<<"\r\n">>, Row | Acc]);
 
 encode_b64(Rest, Acc) -> encode_b64(<<>>, [Rest | Acc]).
 
@@ -71,9 +75,9 @@ base64_test() ->
     BEncode=fun(X)->binary_to_list(iolist_to_binary(encode(base64, X))) end,
     [
      ?assertEqual(
-	  "MTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTEx\n"++
-	  "MTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTEx\n"++
-	  "MTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTEx\n"++
+	  "MTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTEx\r\n"++
+	  "MTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTEx\r\n"++
+	  "MTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTEx\r\n"++
 	  "MTExMTExMTExMTExMTExMTExMTExMTExMQo=", BEncode([string:copies("1",196),$\n])),
      ?assertEqual("SGVsbG8gd29ybGQh", BEncode("Hello world!")),
      ?assertEqual("", BEncode(""))
@@ -90,52 +94,52 @@ quoted_printable_test() ->
      
      ?assertEqual(Txt7++"123456", FlatEncode(Txt7++"123456")),
      ?assertEqual(Txt7++"123=3D", FlatEncode(Txt7++"123=")),
-     ?assertEqual(Txt7++"1234=\n=3D", FlatEncode(Txt7++"1234=")),
-     ?assertEqual(Txt7++"12=3D=\n456", FlatEncode(Txt7++"12=456")),
+     ?assertEqual(Txt7++"1234=\r\n=3D", FlatEncode(Txt7++"1234=")),
+     ?assertEqual(Txt7++"12=3D=\r\n456", FlatEncode(Txt7++"12=456")),
 
-     ?assertEqual(Blank75++"=\n    =20", FlatEncode(Blank75++"     ")),
-     ?assertEqual(Blank75++"=\n=3D  =20", FlatEncode(Blank75++"=   ")),
-     ?assertEqual(Blank75++"=\n=3D", FlatEncode(Blank75++"=")),
+     ?assertEqual(Blank75++"=\r\n    =20", FlatEncode(Blank75++"     ")),
+     ?assertEqual(Blank75++"=\r\n=3D  =20", FlatEncode(Blank75++"=   ")),
+     ?assertEqual(Blank75++"=\r\n=3D", FlatEncode(Blank75++"=")),
 
      ?assertEqual("=0D=0D=0D=0D", FlatEncode("\r\r\r\r")),
-     ?assertEqual("=0D=0D\n", FlatEncode("\r\r\r\n")),
-     ?assertEqual("\n\n\n\n", FlatEncode("\n\n\n\n")),
-     ?assertEqual("\n1\n\n123\n", FlatEncode("\n1\n\n123\n")),
-     ?assertEqual("\n=20\n =09\n\t  =09\n", FlatEncode("\n \n \t\n\t  \t\n")),
-     ?assertEqual("\n1\n\n123\n", FlatEncode("\r\n1\n\n123\r\n")),
+     ?assertEqual("=0D=0D\r\n", FlatEncode("\r\r\r\n")),
+     ?assertEqual("\r\n\r\n\r\n\r\n", FlatEncode("\n\n\n\n")),
+     ?assertEqual("\r\n1\r\n\r\n123\r\n", FlatEncode("\n1\n\n123\n")),
+     ?assertEqual("\r\n=20\r\n =09\r\n\t  =09\r\n", FlatEncode("\n \n \t\n\t  \t\n")),
+     ?assertEqual("\r\n1\r\n\r\n123\r\n", FlatEncode("\r\n1\n\n123\r\n")),
 
-     ?assertEqual(Txt7++"123456\n1234567890\n", FlatEncode(Txt7++"123456\n1234567890\n")),
-     ?assertEqual(Txt7++"123=3D\n1234567890\n", FlatEncode(Txt7++"123=\n1234567890\n")),
-     ?assertEqual(Txt7++"123456\n=3D234567890\n", FlatEncode(Txt7++"123456\n=234567890\n")),
-     ?assertEqual(Txt7++"123=3D\n=3D234567890\n", FlatEncode(Txt7++"123=\n=234567890\n")),
+     ?assertEqual(Txt7++"123456\r\n1234567890\r\n", FlatEncode(Txt7++"123456\n1234567890\n")),
+     ?assertEqual(Txt7++"123=3D\r\n1234567890\r\n", FlatEncode(Txt7++"123=\n1234567890\n")),
+     ?assertEqual(Txt7++"123456\r\n=3D234567890\r\n", FlatEncode(Txt7++"123456\n=234567890\n")),
+     ?assertEqual(Txt7++"123=3D\r\n=3D234567890\r\n", FlatEncode(Txt7++"123=\n=234567890\n")),
 
-     ?assertEqual(Txt7++"12345=\n6=3D890\n", FlatEncode(Txt7++"123456=890\n")),
-     ?assertEqual(Txt7++"12345=\n=3D7890\n", FlatEncode(Txt7++"12345=7890\n")),
-     ?assertEqual(Txt7++"1234=\n=3D67890\n", FlatEncode(Txt7++"1234=67890\n")),
-     ?assertEqual(Txt7++"123=\n=3D567890\n", FlatEncode(Txt7++"123=567890\n")),
-     ?assertEqual(Txt7++"12=3D=\n4567890\n", FlatEncode(Txt7++"12=4567890\n")),
-     ?assertEqual(Txt7++"1=3D3=\n4567890\n", FlatEncode(Txt7++"1=34567890\n")),
+     ?assertEqual(Txt7++"12345=\r\n6=3D890\r\n", FlatEncode(Txt7++"123456=890\n")),
+     ?assertEqual(Txt7++"12345=\r\n=3D7890\r\n", FlatEncode(Txt7++"12345=7890\n")),
+     ?assertEqual(Txt7++"1234=\r\n=3D67890\r\n", FlatEncode(Txt7++"1234=67890\n")),
+     ?assertEqual(Txt7++"123=\r\n=3D567890\r\n", FlatEncode(Txt7++"123=567890\n")),
+     ?assertEqual(Txt7++"12=3D=\r\n4567890\r\n", FlatEncode(Txt7++"12=4567890\n")),
+     ?assertEqual(Txt7++"1=3D3=\r\n4567890\r\n", FlatEncode(Txt7++"1=34567890\n")),
 
-     ?assertEqual(Txt7++"12345=\n6=3D=3D90\n", FlatEncode(Txt7++"123456==90\n")),
-     ?assertEqual(Txt7++"12345=\n=3D=3D890\n", FlatEncode(Txt7++"12345==890\n")),
-     ?assertEqual(Txt7++"1234=\n=3D=3D7890\n", FlatEncode(Txt7++"1234==7890\n")),
-     ?assertEqual(Txt7++"123=\n=3D=3D67890\n", FlatEncode(Txt7++"123==67890\n")),
-     ?assertEqual(Txt7++"12=3D=\n=3D567890\n", FlatEncode(Txt7++"12==567890\n")),
-     ?assertEqual(Txt7++"1=3D=\n=3D4567890\n", FlatEncode(Txt7++"1==4567890\n")),
-     ?assertEqual(Txt7++"=3D=\n=3D34567890\n", FlatEncode(Txt7++"==34567890\n")),
-     ?assertEqual(Txt6++"123456789=3D=3D=\n234567890\n", FlatEncode(Txt6++"123456789==234567890\n")),
-     ?assertEqual(Txt6++"12345678=3D=3D1=\n234567890\n", FlatEncode(Txt6++"12345678==1234567890\n")),
+     ?assertEqual(Txt7++"12345=\r\n6=3D=3D90\r\n", FlatEncode(Txt7++"123456==90\n")),
+     ?assertEqual(Txt7++"12345=\r\n=3D=3D890\r\n", FlatEncode(Txt7++"12345==890\n")),
+     ?assertEqual(Txt7++"1234=\r\n=3D=3D7890\r\n", FlatEncode(Txt7++"1234==7890\n")),
+     ?assertEqual(Txt7++"123=\r\n=3D=3D67890\r\n", FlatEncode(Txt7++"123==67890\n")),
+     ?assertEqual(Txt7++"12=3D=\r\n=3D567890\r\n", FlatEncode(Txt7++"12==567890\n")),
+     ?assertEqual(Txt7++"1=3D=\r\n=3D4567890\r\n", FlatEncode(Txt7++"1==4567890\n")),
+     ?assertEqual(Txt7++"=3D=\r\n=3D34567890\r\n", FlatEncode(Txt7++"==34567890\n")),
+     ?assertEqual(Txt6++"123456789=3D=3D=\r\n234567890\r\n", FlatEncode(Txt6++"123456789==234567890\n")),
+     ?assertEqual(Txt6++"12345678=3D=3D1=\r\n234567890\r\n", FlatEncode(Txt6++"12345678==1234567890\n")),
 
-     ?assertEqual(Txt7++"12345=\n67890\n", FlatEncode(string:copies(Txt,8)++"\n")),
-     ?assertEqual(Txt7++"123456\n", FlatEncode(Txt7++"123456\n")),
+     ?assertEqual(Txt7++"12345=\r\n67890\r\n", FlatEncode(string:copies(Txt,8)++"\n")),
+     ?assertEqual(Txt7++"123456\r\n", FlatEncode(Txt7++"123456\n")),
 
      ?assertEqual(
-	Txt7++"12345=\n67890"++Txt7++"=\n"++Txt7++"12345=\n67890"++Txt7++"=\nTobias L=C3=B6fgren =C3=85=C3=84=C3=96 "++string:copies(Txt,3)++"123456=\n7890"++string:copies(Txt,3)++"1234=3D//=3D-=3D567890"++Txt++Txt++"\n", 
+	Txt7++"12345=\r\n67890"++Txt7++"=\r\n"++Txt7++"12345=\r\n67890"++Txt7++"=\r\nTobias L=C3=B6fgren =C3=85=C3=84=C3=96 "++string:copies(Txt,3)++"123456=\r\n7890"++string:copies(Txt,3)++"1234=3D//=3D-=3D567890"++Txt++Txt++"\r\n", 
 	FlatEncode(string:copies(Txt,30)++"Tobias Löfgren ÅÄÖ "++Txt7++"1234=//=-=567890"++Txt++Txt++"\n")),
 
      ?assertEqual("=3D=3D=3D=3D=3D", FlatEncode("=====")),
-     ?assertEqual("Hej\nd=C3=A5!", FlatEncode("Hej\ndå!")),
-     ?assertEqual("Test of \twhitespace,\t=20\nbefore newline.\n", FlatEncode("Test of \twhitespace,\t \nbefore newline.\n")),
+     ?assertEqual("Hej\r\nd=C3=A5!", FlatEncode("Hej\ndå!")),
+     ?assertEqual("Test of \twhitespace,\t=20\r\nbefore newline.\r\n", FlatEncode("Test of \twhitespace,\t \nbefore newline.\n")),
      ?assertEqual("Tobias L=C3=B6fgren", FlatEncode("Tobias Löfgren")),
      ?assertEqual("Tobias Lofgren", FlatEncode("Tobias Lofgren"))
     ].
